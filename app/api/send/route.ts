6af767325a21ccf33b2bdf17e8288877808f53bb
Ali Hussein <42802922+Ali-Hussein-dev@users.noreply.github.com>
2024-04-03 00:07:24 +0200

    Update route.ts

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Ali support@indie-starter.dev',
+      from: 'support@indie-starter.dev',
       to: ['a.m.housen@gmail.com'],
       subject: "Testing resend",
       react: EmailTemplate({ firstName: "Ali" }) as React.ReactElement,
